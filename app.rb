a363be2dddc8d6950fc5ca01111a14e7b623469f
rasdinho <ras.dinho7@gmail.com>
2020-03-30 14:33:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+	
+	erb :index #this tells sinatra to load the html file in the views file
 	end
+
+
+    get "/info" do
+		erb :info
+	  end
 end
